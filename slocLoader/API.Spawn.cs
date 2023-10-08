534db215aaa4f45f957b8e417f8817adb9b6acf4
Axwabo <axwabo@gmail.com>
2023-10-08 14:45:22 +0200

    spawn

--- slocLoader/API.Spawn.cs
+++ slocLoader/API.Spawn.cs
@@ -1,4 +1,5 @@
 ﻿using Mirror;
+using slocLoader.ObjectCreation;
 using slocLoader.Objects;
 
 namespace slocLoader;
@@ -6,10 +7,46 @@ namespace slocLoader;
 public static partial class API
 {
 
-    public static GameObject SpawnObjects(IEnumerable<slocGameObject> objects, Vector3 position, Quaternion rotation = default) =>
-        SpawnObjects(objects, out _, position, rotation);
+    #region Deprecated methods
 
+    [Obsolete("Use SpawnObjects(ObjectSource, Vector3, Quaternion) instead")]
+    public static GameObject SpawnObjects(IEnumerable<slocGameObject> objects, Vector3 position, Quaternion rotation = default)
+        => SpawnObjects(objects, out _, position, rotation);
+
+    [Obsolete("Use SpawnObjects(ObjectSource, out int, Vector3, Quaternion) instead")]
     public static GameObject SpawnObjects(IEnumerable<slocGameObject> objects, out int spawnedAmount, Vector3 position, Quaternion rotation = default)
+        => SpawnObjects(ObjectsSource.From(objects), new CreateOptions
+        {
+            Position = position,
+            Rotation = rotation
+        }, out spawnedAmount);
+
+    [Obsolete("Use SpawnObjects(ObjectSource, out int, Vector3, Quaternion) instead")]
+    public static GameObject SpawnObjectsFromStream(Stream objects, out int spawnedAmount, Vector3 position, Quaternion rotation = default)
+        => SpawnObjects(ReadObjects(objects), out spawnedAmount, position, rotation);
+
+    [Obsolete("Use SpawnObjects(ObjectSource, out int, Vector3, Quaternion) instead")]
+    public static GameObject SpawnObjectsFromFile(string path, out int spawnedAmount, Vector3 position, Quaternion rotation = default)
+        => SpawnObjects(ReadObjectsFromFile(path), out spawnedAmount, position, rotation);
+
+    #endregion
+
+    public static GameObject SpawnObject(this slocGameObject obj, GameObject parent = null, bool throwOnError = true)
+    {
+        var o = CreateObject(obj, parent, throwOnError);
+        if (o == null)
+        {
+            if (throwOnError)
+                throw new ArgumentOutOfRangeException(nameof(obj.Type), obj.Type, "Unknown object type");
+            return null;
+        }
+
+        if (!o.TryGetComponent(out slocObjectData data) || data.ShouldBeSpawnedOnClient)
+            NetworkServer.Spawn(o);
+        return o;
+    }
+
+    public static GameObject SpawnObjects(ObjectsSource source, CreateOptions options, out int spawnedAmount)
     {
         CreatedInstances.Clear();
         TpToSpawnedCache.Clear();
@@ -19,15 +56,15 @@ public static partial class API
             {
                 transform =
                 {
-                    position = position,
-                    rotation = rotation,
+                    position = options.Position,
+                    rotation = options.Rotation,
                 }
             };
             go.AddComponent<NetworkIdentity>();
             go.AddComponent<slocObjectData>();
             NetworkServer.Spawn(go);
             spawnedAmount = 0;
-            foreach (var o in objects)
+            foreach (var o in source)
             {
                 var gameObject = o.SpawnObject(CreatedInstances.GetOrReturn(o.ParentId, go, o.HasParent));
                 if (gameObject == null)
@@ -46,25 +83,17 @@ public static partial class API
         }
     }
 
-    public static GameObject SpawnObjectsFromStream(Stream objects, out int spawnedAmount, Vector3 position, Quaternion rotation = default)
-        => SpawnObjects(ReadObjects(objects), out spawnedAmount, position, rotation);
+    public static GameObject SpawnObjects(ObjectsSource source, Vector3 position, Quaternion rotation = default)
+        => SpawnObjects(source, out _, position, rotation);
 
-    public static GameObject SpawnObjectsFromFile(string path, out int spawnedAmount, Vector3 position, Quaternion rotation = default)
-        => SpawnObjects(ReadObjectsFromFile(path), out spawnedAmount, position, rotation);
-
-    public static GameObject SpawnObject(this slocGameObject obj, GameObject parent = null, bool throwOnError = true)
-    {
-        var o = CreateObject(obj, parent, throwOnError);
-        if (o == null)
+    public static GameObject SpawnObjects(ObjectsSource source, out int spawnedAmount, Vector3 position, Quaternion rotation = default)
+        => SpawnObjects(source, new CreateOptions
         {
-            if (throwOnError)
-                throw new ArgumentOutOfRangeException(nameof(obj.Type), obj.Type, "Unknown object type");
-            return null;
-        }
+            Position = position,
+            Rotation = rotation
+        }, out spawnedAmount);
 
-        if (!o.TryGetComponent(out slocObjectData data) || data.ShouldBeSpawnedOnClient)
-            NetworkServer.Spawn(o);
-        return o;
-    }
+    public static GameObject SpawnObjects(ObjectsSource source, CreateOptions options)
+        => SpawnObjects(source, options, out _);
 
 }
