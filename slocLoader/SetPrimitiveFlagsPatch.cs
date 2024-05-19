0907c6185183ca76ac132d352f1534cf8c780b55
Axwabo <axwabo@gmail.com>
2024-05-19 10:26:19 +0200

    server-side collider fix

---
+++ slocLoader/SetPrimitiveFlagsPatch.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection.Emit;
+using AdminToys;
+using HarmonyLib;
+using static Axwabo.Helpers.Harmony.InstructionHelper;
+
+namespace slocLoader;
+
+[HarmonyPatch(typeof(PrimitiveObjectToy), nameof(PrimitiveObjectToy.SetFlags))]
+internal static class SetPrimitiveFlagsPatch
+{
+
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var list = new List<CodeInstruction>(instructions);
+        var start = list.FindCode(OpCodes.Ldarg_2);
+        var end = list.FindCall("set_enabled");
+        list.RemoveRange(start, end - start);
+        list.InsertRange(start, new[]
+        {
+            This,
+            Call(SetPrimitiveTypePatch.ShouldAddCollider)
+        });
+        return list;
+    }
+
+}
